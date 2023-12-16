b8fdcce753c9a11126586c3bf7e4a4edb879da7b
HUYVO <huyvo0188630@gmail.com>
2023-12-16 13:43:30 +0700

    Upload frist

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import UserList from './UserList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Ứng dụng React đơn giản</h1>
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
